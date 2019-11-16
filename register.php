15a6dddddcc8eb60450b4650c7b9a86a80273a65
vinibispo <vini.bispo015@gmail.com>
2019-11-16 14:37:50 -0300

    basic register

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registrar</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="nome" id="nome" placeholder="Nome">
+        <input type="email" name="email" id="email" placeholder="Email">
+        <input type="password" name="senha" id="senha" placeholder="Senha">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
